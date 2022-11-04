3af9a4ae2870ef9112c092510941c0023680cad6
ok2ju <soulSpringg@gmail.com>
2022-11-04 11:00:41 +0300

    feature: Add raw service logger

--- service/app.py
+++ service/app.py
@@ -1,41 +1,60 @@
-from flask import Flask, request
+import logging
+
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from models import db, MindModel
 
 app = Flask(__name__)
-app.config.from_pyfile("settings.py")
+app.config.from_pyfile('settings.py')
 CORS(app)
+logging.basicConfig(filename='logs.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 db.init_app(app)
 migrate = Migrate(app, db)
 
-@app.route("/healthcheck")
-def print_hello():
-    return "Hello, Flask!"
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not found: ' + request.url
+    }
+    response = jsonify(message)
+    response.status_code = 404
 
-@app.route("/api/v1/records", methods = ["GET"])
+    return response
+
+@app.route('/healthcheck')
+def print_hello():
+    app.logger.info('Application is up and running')
+    return 'Hello, Flask!'
+
+@app.route('/api/v1/records', methods = ['GET'])
 def get_records():
     records = MindModel.query.all()
     results = [
         {
-            "id": mind.id,
-            "mood": mind.mood,
-            "energy": mind.energy,
-            "date": mind.date
+            'id': mind.id,
+            'mood': mind.mood,
+            'energy': mind.energy,
+            'date': mind.date
         } for mind in records]
 
-    return { "count": len(results), "entities": results }
+    app.logger.info('Requested records:', len(results))
 
-@app.route("/api/v1/records", methods = ["POST"])
+    return { 'count': len(results), 'entities': results }
+
+@app.route('/api/v1/records', methods = ['POST'])
 def add_record():
     data = request.get_json()
-    record = MindModel(mood = data["mood"], energy = data["energy"])
+    record = MindModel(mood = data['mood'], energy = data['energy'])
     db.session.add(record)
     db.session.commit()
 
+    app.logger.info('Created a new record')
+
     return data
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug = True)
